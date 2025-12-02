5148a07b899226843ab6c2c5b8fdd78d5463355a
soko <sajidaaitsimhand1@gmail.com>
2025-12-03 00:31:51 +0100

    bigint

--- lvl1/Bigint/bigint.hpp
+++ lvl1/Bigint/bigint.hpp
@@ -1,50 +1,5 @@
 
-// #pragma once 
-
-// #include <string>
-// #include <iostream>
-
-// class bigint {
-// private:
-//     std::string digits;
-//     std::string addStrings(const std::string& a, const std::string& b) const;
-//     void removeLeadingZeros();
-//     bool isZero() const;
-    
-// public:
-//     bigint();
-//     bigint(unsigned int num);
-//     bigint(const std::string& str);
-    
-//     bigint operator+(const bigint& other) const;
-//     bigint& operator+=(const bigint& other);
-    
-//     bool operator==(const bigint& other) const;
-//     bool operator!=(const bigint& other) const;
-//     bool operator<(const bigint& other) const;
-//     bool operator>(const bigint& other) const;
-//     bool operator<=(const bigint& other) const;
-//     bool operator>=(const bigint& other) const;
-    
-//     bigint operator<<(unsigned int shift) const;
-//     bigint operator>>(unsigned int shift) const;
-//     bigint& operator<<=(unsigned int shift);
-//     bigint& operator>>=(unsigned int shift);
-    
-//     // shift with bigint
-//     bigint operator<<(const bigint& shift) const;
-//     bigint operator>>(const bigint& shift) const;
-//     bigint& operator<<=(const bigint& shift);
-//     bigint& operator>>=(const bigint& shift);
-
-//     bigint& operator++();     
-//     bigint operator++(int);    
-    
-//     std::string getDigits() const;
-// };
-// std::ostream& operator<<(std::ostream& os, const bigint& num);
 #pragma once
-
 #include <iostream>
 #include <sstream>
 #include <string>
