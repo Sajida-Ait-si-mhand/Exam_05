766746722acf343f79b42030cf248ed6752dd154
soko <sajidaaitsimhand1@gmail.com>
2026-01-11 23:29:15 +0100

    oussy

--- lvl1/Bigint/bigint.hpp
+++ lvl1/Bigint/bigint.hpp
@@ -1,222 +1,158 @@
-
 #pragma once
+
 #include <iostream>
-#include <sstream>
+#include <cstdlib>
 #include <string>
-#include <algorithm>
-#include <cctype>
-#include <climits>
 
-class bigint {
-private:
-    std::string digits;
+class bigint
+{
+	std::string digits;
+	public:
+		bigint(unsigned long n = 0)
+		{
+			if(n == 0)
+				digits = "0";
+			else
+			{
+				while (n> 0 )
+				{
+					digits.push_back((n % 10) + '0');
+					n /= 10;
+				}
+				
+			}
+		}
+		bigint(const std::string& n)
+		{
+			std::string res =n;
+			std::reverse(digits.begin() ,digits.end());
 
-    std::string addStrings(const std::string& a, const std::string& b) const {
-        std::string result;
-        int carry = 0;
-        size_t len = std::max(a.size(), b.size());
-        
-        for (size_t i = 0; i < len || carry; ++i) {
-            int digitA = i < a.size() ? a[i] - '0' : 0;
-            int digitB = i < b.size() ? b[i] - '0' : 0;
-            int sum = digitA + digitB + carry;
-            result.push_back((sum % 10) + '0');
-            carry = sum / 10;
-        }
-        
-        return result;
-    }
+			while (digits.length() > 1 && digits.back() == '0')
+			{
+				digits.pop_back();
+			}
+			if(digits.empty())
+				digits = "0";
+			
+			
+		}
+		bigint(const bigint& other) : digits(other.digits){}
+		bigint& operator=(const bigint& other) 
+		{
+			if(this != &other)
+			{
+				digits = other.digits;
+			}
+			return *this;
+		}
+		bigint& operator+=(const bigint& other) 
+		{
+			int carry = 0;
+			std::string res;
+			for(size_t i = 0; i< digits.length() || i< other.digits.length()|| carry; ++i)
+			{
+				int d1 = i < digits.length()? digits[i] - '0' : 0;
+				int d2 = i < other.digits.length()? other.digits[i] - '0' : 0;
+				int sum = d1 +d2 + carry;
+				res.push_back((sum % 10) +'0' );
+				carry = sum / 10;
+			}
+			digits = res;
+			return *this;
+		}
+		bigint operator+(const bigint& other) const
+		{
+			return bigint(*this) += other;
+		}
 
-    void removeLeadingZeros() {
-        while (digits.size() > 1 && digits.back() == '0') {
-            digits.pop_back();
-        }
-    }
+		bigint& operator++()
+		{
+			return *this += bigint(1);
+		}
+		bigint operator++(int)
+		{
+			bigint tmp  = *this;
+			++(*this);
+			return tmp;
+		}
+		int to_int(const bigint& other) const
+		{
+			std::string res = other.digits;
+			std::reverse(res.begin(), res.end());
+			int num  = std::atoi(res.c_str());
 
-    bool isZero() const {
-        return digits == "0";
-    }
+			return num;
+		}
+		bigint& operator<<=(const bigint& other)
+		{
+			int n = to_int(other);
+			if(digits != "0" && n >0)
+			{
+				digits.insert(0 ,n ,'0');
+			}
+			return *this;
+		}
+		bigint operator<<(const bigint& other) const
+		{
+			return bigint(*this) <<= other;
+		}
+		bigint& operator>>=(const bigint& other)
+		{
+			int n = to_int(other);
+			if(n > 0)
+			{
+				if((size_t)n >= digits.length())
+					digits = "0";
+				else
+					digits.erase(0,n);
+			}
+			if(digits.empty())
+				digits = "0";
+			return *this ;
+		}
+		bigint operator>>(const bigint& other) const
+		{
+			return bigint(*this) >>= other;
+		}
+		bool operator<(const bigint& other) const
+		{
+			if(digits.length() != other.digits.length())
+				return digits.length() < other.digits.length();
+			for( int i = digits.length() - 1  ; i >= 0; --i)
+			{
+				if(digits[i] != other.digits[i])
+					return digits[i] <other.digits[i];
 
-    std::string getDigits() const {
-        return digits;
-    }
+			}
+			return false;
+		}
+		bool operator>=(const bigint& other) const
+		{
+			return !(*this < other);
+		}
+		bool operator>(const bigint& other) const
+		{
+			return other < *this;;
+		}
+		bool operator<=(const bigint& other) const
+		{
+			return !(other < *this);
+		}
+		bool operator==(const bigint& other) const
+		{
+			return digits == other.digits;
+		}
+		bool operator!=(const bigint& other) const
+		{
+			return !(*this == other);
+		}
 
-public:
-    bigint() : digits("0") {}
-
-    // bigint(unsigned long long num) {
-    //     if (num == 0) {
-    //         digits = "0";
-    //         return;
-    //     }
-    //     std::ostringstream oss;
-    //     oss << num;
-    //     digits = oss.str();
-    //     std::reverse(digits.begin(), digits.end());
-    // }
-
-    // bigint(const std::string& str) {
-    //     if (str.empty() || !std::all_of(str.begin(), str.end(), ::isdigit)) {
-    //         digits = "0";
-    //     } else {
-    //         digits = str;
-    //         std::reverse(digits.begin(), digits.end());
-    //         removeLeadingZeros();
-    //     }
-    // }
-    bigint(unsigned long long num){
-    if(num == 0)
-    {
-        digits = "0";
-        return;
-    }
-    digits = std::to_string(num);
-    std::reverse(digits.begin(), digits.end());
-    };
-	// str => revese
-	bigint (const std::string &str)
-	{
-		digits = str;
-		std::reverse(digits.begin(), digits.end());
-        removeLeadingZeros();
-	}
-    bigint operator+(const bigint& other) const {
-        bigint result;
-        result.digits = addStrings(this->digits, other.digits);
-        result.removeLeadingZeros();
-        return result;
-    }
-
-    bigint& operator+=(const bigint& other) {
-        digits = addStrings(this->digits, other.digits);
-        removeLeadingZeros();
-        return *this;
-    }
-
-    bool operator==(const bigint& other) const {
-        bigint a = *this, b = other;
-        a.removeLeadingZeros();
-        b.removeLeadingZeros();
-        return a.digits == b.digits;
-    }
-
-    bool operator!=(const bigint& other) const {
-        return !(*this == other);
-    }
-
-    bool operator<(const bigint& other) const {
-        bigint a = *this, b = other;
-        a.removeLeadingZeros();
-        b.removeLeadingZeros();
-        
-        if (a.digits.size() != b.digits.size())
-            return a.digits.size() < b.digits.size();
-        
-        for (int i = a.digits.size() - 1; i >= 0; --i) {
-            if (a.digits[i] != b.digits[i])
-                return a.digits[i] < b.digits[i];
-        }
-        return false;
-    }
-
-    bool operator>(const bigint& other) const { 
-        return other < *this; 
-    }
-
-    bool operator<=(const bigint& other) const { 
-        return !(*this > other); 
-    }
-
-    bool operator>=(const bigint& other) const { 
-        return !(*this < other); 
-    }
-
-    bigint operator<<(unsigned int shift) const {
-        if (isZero() || shift == 0) return *this;
-        
-        bigint result = *this;
-        result.digits.insert(result.digits.begin(), shift, '0');
-        result.removeLeadingZeros();
-        return result;
-    }
-
-    bigint operator>>(unsigned int shift) const {
-        if (shift == 0) return *this;
-        
-        bigint result = *this;
-        if (shift >= result.digits.size())  // ← BUG HERE!
-            result.digits = "0";
-        else 
-            result.digits.erase(result.digits.begin(), result.digits.begin() + shift);
-        result.removeLeadingZeros();
-    return result;
-}
-
-    bigint& operator<<=(unsigned int shift) {
-        *this = *this << shift;
-        return *this;
-    }
-
-    bigint& operator>>=(unsigned int shift) {
-        *this = *this >> shift;
-        return *this;
-    }
-
-    bigint operator<<(const bigint& shift) const {
-        if (shift.isZero()) return *this;
-        
-        bigint maxShift(UINT_MAX);
-        if (shift > maxShift) {
-            return *this << UINT_MAX;
-        }
-        
-        std::string reversed = shift.digits;
-        std::reverse(reversed.begin(), reversed.end());
-        unsigned int shiftAmount = std::stoul(reversed);
-        
-        return *this << shiftAmount;
-    }
-
-    bigint operator>>(const bigint& shift) const {
-        if (shift.isZero()) return *this;
-        
-        bigint maxShift(UINT_MAX);
-        if (shift > maxShift) {
-            return bigint(0);
-        }
-        
-        std::string reversed = shift.digits;
-        std::reverse(reversed.begin(), reversed.end());
-        unsigned int shiftAmount = std::stoul(reversed);
-        
-        return *this >> shiftAmount;
-    }
-
-    bigint& operator<<=(const bigint& shift) {
-        *this = *this << shift;
-        return *this;
-    }
-
-    bigint& operator>>=(const bigint& shift) {
-        *this = *this >> shift;
-        return *this;
-    }
-
-    bigint operator++(int) {
-        bigint temp = *this;
-        *this += bigint(1);
-        return temp;
-    }
-
-    bigint& operator++() {
-        *this += bigint(1);
-        return *this;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const bigint& num) {
-        std::string str(num.getDigits());
-        std::reverse(str.begin(), str.end());
-        os << str;
-        return os;
-    }
+		friend std::ostream& operator<<(std::ostream& os , const bigint& b)
+		{
+			for(int i = b.digits.length() -1 ; i >= 0; --i)
+			{
+				os << b.digits[i];
+			}
+			return os;
+		}
 };
